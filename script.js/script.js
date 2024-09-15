cfa3416d76f56888f7314d96e78e1ce3e1a2875b
Ferdin123 <maghiferdynad7@gmail.com>
2024-09-15 10:39:00 +0700

    first commit

---
+++ script.js/script.js
@@ -0,0 +1,9 @@
+function shopNow() {
+  window.location.href = "#product";
+}
+
+document.querySelectorAll(".product button").forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("Product added to cart");
+  });
+});
